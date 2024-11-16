05d52fe17d1c6e29e7a8957994052664d52d0ee1
Elijah Retzlaff <eliriver@gmail.com>
2024-11-16 16:49:34 -0500

    first commit

---
+++ sorting.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h> 
+
+// Avoid making changes to this function skeleton, apart from data type changes if required
+// In this starter code we have used uint32_t, feel free to change it to any other data type if required
+void sort_array(uint32_t *arr, size_t size) {
+    // Enter your logic here
+}
+
+int main() {
+    //Initialise the array
+
+    uint32_t *sorted_arr = malloc(size * sizeof(uint32_t)); // Allocate memory for the sorted array
+    
+    // Sort the copied array
+    sort_array(sorted_arr, size);
+
+    // Print the sorted array
+
+    return 0;
+}
+
+       
